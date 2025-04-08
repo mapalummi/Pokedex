1859bd51a0a803a0897ca0581f764ac64496c4ab
Marco <marco.palummieri@gmail.com>
2025-04-08 09:35:16 +0200

    update 01

---
+++ scripts/oldscript.js
@@ -0,0 +1,47 @@
+
+  
+  // function renderImages() {
+  //   let contentRef = document.getElementById("content");
+  //   for (let i = 0; i < images.length; i++) {
+  //     contentRef.innerHTML += getImages(i);
+  //   }
+  // }
+  
+  // function fullSizeImages(i) {
+  //   let imgRef = document.getElementById("img_dialog");
+  //   imgRef.innerHTML = getFullSizeImage(i);
+  
+  //   if (i <= 0) {
+  //     document.getElementById("d_left_button").classList.add("d_none");
+  //   }
+  
+  //   if (i >= 11) {
+  //     document.getElementById("d_right_button").classList.add("d_none");
+  //   }
+  
+  //   document.getElementById("img_dialog").classList.remove("d_none");
+  //   document.getElementById("body_overlay").classList.remove("d_none");
+  // }
+  
+  // function closeDialog() {
+  //   document.getElementById("img_dialog").classList.add("d_none");
+  //   document.getElementById("body_overlay").classList.add("d_none");
+  // }
+  
+  // function leftButtonClick(i) {
+  //   let leftButtonRef = document.getElementById("img_dialog");
+  //   leftButtonRef.innerHTML = leftButton(i)
+  
+  //   if (i <= 1) {
+  //     document.getElementById("d_left_button").classList.add("d_none");
+  //   }
+  // }
+  
+  // function rightButtonClick(i) {
+  //   let rightButtonRef = document.getElementById("img_dialog");
+  //   rightButtonRef.innerHTML = rightButton(i);
+  
+  //   if (i >= 11) {
+  //     document.getElementById("d_right_button").classList.add("d_none");
+  //   }
+  // }
