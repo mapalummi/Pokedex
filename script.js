32ac2cc5fc55f0cf30efc4b569a1d21d08e4fa40
Marco <marco.palummieri@gmail.com>
2025-04-08 15:15:02 +0200

    add new render function

--- script.js
+++ script.js
@@ -1,36 +1,41 @@
 function init(){
-getPokemonData();
+  getPokemonData();
 }
 
+
 async function getPokemonData(){
-  for (let i = 1; i <=10; i++) {
+  for (let i = 1; i <=25; i++) {
     let url = `https://pokeapi.co/api/v2/pokemon/${i}`
     let response = await fetch(url);
     let pokemon = await response.json();
     console.log(pokemon);
 
+    renderMyPokemon(pokemon, i);
+
     for (let j = 0; j < pokemon.types.length; j++) {
       const element = pokemon.types[j];
-      console.log(element);
-  
-    renderMyPokemon(pokemon, element);
+      const pokemonType = element.type.name;
+      console.log(pokemonType);
+
+      let typeContent = document.getElementById(`type${i}`);
+      typeContent.innerHTML += /*html*/`
+        <p>${pokemonType}</p>
+      `
+    }
   }
 }
 
 
-
-
-function renderMyPokemon(pokemon, element){
-    let pokemonContent = document.getElementById('pokemon_content');
-    pokemonContent.innerHTML += /*html*/`
-          <div class="card">
-            <div class="card_content">
-              <p># ${pokemon.id}</p>
-              <p>Name: ${pokemon.name}</p>
-              <p>Type: ${element.type.name}</p>
-              <img src="${pokemon.sprites.front_default}">
+function renderMyPokemon(pokemon, index){
+      let pokemonContent = document.getElementById('pokemon_content');
+      pokemonContent.innerHTML += /*html*/`
+            <div class="card">
+              <div class="card_content">
+                <p># ${pokemon.id}</p>
+                <p>Name: ${pokemon.name}</p>
+                <div id="type${index}">Type:</div>
+                <img src="${pokemon.sprites.front_default}">
+              </div>
             </div>
-          </div>
-      `;
-    }
+        `;  
 }
