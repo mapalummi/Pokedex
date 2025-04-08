9819d24b9fcc88528a73232d241fc859724d5de0
Marco <marco.palummieri@gmail.com>
2025-04-08 20:03:00 +0200

    add new function for classlist

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-function init(){
+function init() {
   getPokemonData();
 }
 
-
-async function getPokemonData(){
-  for (let i = 1; i <=25; i++) {
-    let url = `https://pokeapi.co/api/v2/pokemon/${i}`
+async function getPokemonData() {
+  for (let i = 1; i <= 25; i++) {
+    let url = `https://pokeapi.co/api/v2/pokemon/${i}`;
     let response = await fetch(url);
     let pokemon = await response.json();
 
@@ -16,36 +15,50 @@ async function getPokemonData(){
       const pokemonType = element.type.name;
 
       let typeContent = document.getElementById(`type${i}`);
-      typeContent.innerHTML += /*html*/`
+      typeContent.innerHTML += /*html*/ `
         <p>${pokemonType}</p>
-      `
-      // let typeDialogContent = document.getElementById(`typeDialog${i}`);
-      // typeDialogContent.innerHTML += /*html*/`
-      //   <p>${pokemonType}</p>
-      // `
+      `;
+      
+      getTypeColor(pokemonType);
     }
   }
 }
 
-
-function renderMyPokemon(pokemon, index){
-      let pokemonContent = document.getElementById('pokemon_content');
-      pokemonContent.innerHTML += /*html*/`
-            <div class="card" onclick="showCard(${index}, '${pokemon.name}', '${pokemon.sprites.other.showdown.front_shiny}', ${pokemon.id})">
+function renderMyPokemon(pokemon, index) {
+  let pokemonContent = document.getElementById("pokemon_content");
+  pokemonContent.innerHTML += /*html*/ `
+            <div id="pokeCard" class="card_white" onclick="showDialogCard(${index}, '${pokemon.name}', '${pokemon.sprites.other.showdown.front_shiny}', ${pokemon.id})">
               <div class="card_content">
-                <p>#${pokemon.id}</p>
-                <p>${pokemon.name}</p>
-                <div id="type${index}"></div>
-                <img src="${pokemon.sprites.front_default}">
+              <div class="id_container"><p>#${pokemon.id}</p></div>
+              <div class="name_container"><p>${pokemon.name}</p></div>
+              <div id="type${index}" class="type_container"></div>
+              <div class="img_container"><img src="${pokemon.sprites.front_default}"></div>
               </div>
             </div>
-        `;  
+        `;
+
+        
+}
+
+//Funktioniert noch nicht richtig:
+function getTypeColor(pokemonType){
+  let typeColor = pokemonType;
+  console.log(pokemonType);
+
+  let standardClass = document.getElementById('pokeCard')
+
+  if (typeColor === 'grass') {
+    console.log('Background grün färben');
+    standardClass.classList.add("card_green")
+  }
 }
 
 
-function showCard(index, name, sprite, id){
-  let pokemonCard = document.getElementById('pokemon_dialog');
-  pokemonCard.innerHTML = /*html*/`
+
+
+function showDialogCard(index, name, sprite, id) {
+  let pokemonCard = document.getElementById("pokemon_dialog");
+  pokemonCard.innerHTML = /*html*/ `
     <div class="dialog">
               <div class="">
                 <p>${name}</p>
@@ -54,18 +67,41 @@ function showCard(index, name, sprite, id){
                 <div id="type${index}"></div>
               </div>
             </div>
-  `
-    document.getElementById("pokemon_dialog").classList.remove("d_none");
-    document.getElementById("body_overlay").classList.remove("d_none");
+  `;
+  document.getElementById("pokemon_dialog").classList.remove("d_none");
+  document.getElementById("body_overlay").classList.remove("d_none");
 
-    document.documentElement.style.overflow = 'hidden';
-    document.body.scroll = "no";
-  }
+  document.documentElement.style.overflow = "hidden";
+  document.body.scroll = "no";
+}
 
-  function closeDialog() {
-    document.getElementById("pokemon_dialog").classList.add("d_none");
-    document.getElementById("body_overlay").classList.add("d_none");
+function closeDialog() {
+  document.getElementById("pokemon_dialog").classList.add("d_none");
+  document.getElementById("body_overlay").classList.add("d_none");
 
-    document.documentElement.style.overflow = 'scroll';
-    document.body.scroll = "yes";
-  }
+  document.documentElement.style.overflow = "scroll";
+  document.body.scroll = "yes";
+}
+
+
+
+
+
+
+
+
+
+
+// Kann evtl raus:
+// document.addEventListener('DOMContentLoaded', (event) => {
+//   let element = document.getElementById("type${index}");
+//   let text = element.textContent || element.innerText;
+
+//   if (text.includes("grass")) {
+//       element.classList.add("card_green");
+//       element.classList.remove("card_white");
+//   } else {
+//       element.classList.add("card_white");
+//       element.classList.remove("card_green");
+//   }
+// });
